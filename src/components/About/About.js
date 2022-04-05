e4292e86860ec089d17863a67b8511832bdbfc3f
danielNewaz <danielnwz420@gmail.com>
2022-04-05 00:59:18 -0700

    added about

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,7 +3,13 @@ import React from 'react';
 const About = () => {
     return (
         <div>
-            <h2>Abouts</h2>
+            <h1 className='text-3xl font-bold mt-5'>What is special about Guitar</h1>
+            <h2 className='text-2xl font-extrabold font-serif text-gray-500 mt-5'>The guitar is a polyphonic instrument. This means that it is capable of playing more than one tone at a time, which means that it can be used to play harmonies. The only other popular instrument that excels at this is the piano. And of the two, the guitar is much easier to transport—and usually much cheaper to buy.
+            <br />
+            Guitar is one of the most popular and widely used instruments.
+
+            The guitar is popular because it is portable, flexible, polyphonic and approachable. The guitar’s popularity makes it an obvious choice for many who wish to learn an instrument. And due to its popularity, there are many   materials available to learn with.
+            </h2>
         </div>
     );
 };
